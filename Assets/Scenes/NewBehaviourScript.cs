9c51d7ed3788c27ee75ea217ce8667e9dfac9d8b
Rafał Majchrzak <r.majchrzak50@gmail.com>
2023-05-07 14:59:37 +0200

    hihi

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //hyh
+        //hihhih
     }
 
     // Update is called once per frame
